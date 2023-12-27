bc23a91b05700d4eddb70921c6af8a31484fd0a9
JFMajer <jakubmajer@live.com>
2023-12-27 20:23:04 +0100

    connecting to postgres db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 const port = 3000
 
 type application struct {
 	Domain string
+	DB     *sql.DB
 }
 
 func main() {
-	app := &application {
-		Domain: "example.com",
+	dsn := "host=localhost user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC"
+
+	// Open a database connection
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Check DB connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	app := &application{
+		Domain: "example.com",
+		DB:     db,
+	}
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		panic(err)
 	}
